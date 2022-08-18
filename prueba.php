6cc4ad074d133bf7042c1a91e1b3fe622aac5ba8
juanjo003 <jjmlj10@gmail.com>
2022-08-18 23:57:45 +0200

    nuevos cambio

--- prueba.php
+++ prueba.php
@@ -1,12 +1,32 @@
 <?php
-$nombre=$_GET['nombre'];
-$name="juanjose";
-setcookie("nueva",$name,time()+365);
-echo $_COOKIE['nueva'];
-/*if (strcmp($nombre,$cadena)==0) {
-	echo "hola usuario";
+session_start();
+if (session_id()) {
+$_SESSION['session_id'] =$_POST['usuario'];
+$_SESSION['contraseña'] =$_POST['contra']; 
+$_SESSION['secion'] =session_id();
+$_SESSION['usuario'] =date("Y-m-dates");
+header("Location:menu2.html");
 }else{
-	echo "adios";
-}*/
+	
+}
 
+foreach ($_SESSION as $key => $val) {
+	echo $key."=".$val;
+	echo "<br>";
+}
+
+
+/*$aux=0;
+$num=0;
+for ($i=0; $i <strlen($nombre) ; $i++) { 
+	if ($nombre[$i]=='@'||$nombre[$i]=='.') {
+		$aux++;
+	}if ($aux==0) {
+			$num= $i;
+		}
+}
+echo substr($nombre,0,$num+1);
+$_SESSION['nuevo']=$nombre;
+
+echo $_SESSION["nuevo"];*/
 ?>
