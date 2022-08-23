eeda12a709823d59fdb480bcb96d93d8965ada46
juanjouser <jjmlj10@gmail.com>
2022-08-24 01:38:02 +0200

    'cambios'

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <h2>hola esto es la nueva pagina</h2>
+  <form action="prueba.php" method="post">
+    <input type="submit" name="salir" value="salir">
+  </form>
 </body>
 </html>
